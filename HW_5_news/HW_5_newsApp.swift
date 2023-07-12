43122ef1572d3e4c9b2390d1b650013b8ea93ed1
dsail <dsail@yandex.ru>
2023-07-12 21:33:03 +0300

    Initial Commit

---
+++ HW_5_news/HW_5_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_5_newsApp.swift
+//  HW_5_news
+//
+//  Created by Eugene Zvyagin on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_5_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
